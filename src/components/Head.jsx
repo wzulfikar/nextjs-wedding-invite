576d8fa5652c4be1590ed58c886f27c9ed7ed93e
wzulfikar <me@wzulfikar.com>
2020-01-25 19:36:50 +0800

    chore(assets): add asset files

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -17,7 +17,6 @@ export default ({ title, description, url, logo, author, siteName, publishedTime
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" href="/assets/images/favicon.png?123" type="image/png" />
-      <link rel="stylesheet" href="/assets/css/magnific-popup.css" />
       <link rel="stylesheet" href="/assets/css/animate.css" />
       <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
       <link rel="stylesheet" href="/assets/css/default.css" />
@@ -27,13 +26,9 @@ export default ({ title, description, url, logo, author, siteName, publishedTime
       {/* [if lt IE 9]> */}
       <script src="/assets/js/vendor/jquery-1.12.4.min.js"></script>
       <script src="/assets/js/vendor/modernizr-3.7.1.min.js"></script>
-      <script src="/assets/js/popper.min.js"></script>
       <script src="/assets/js/bootstrap.min.js"></script>
-      <script src="/assets/js/jquery.magnific-popup.min.js"></script>
       <script src="/assets/js/jquery.easing.min.js"></script>
       <script src="/assets/js/scrolling-nav.js"></script>
-      <script src="/assets/js/wow.min.js"></script>
-      <script src="/assets/js/main.js"></script>
     </Head >
   );
 };
