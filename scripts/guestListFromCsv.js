2a635f9d1fe14b856e2cbb3599b9dee065af4635
wzulfikar <me@wzulfikar.com>
2020-01-26 12:17:20 +0800

    fix(guest-list): fix typo

--- scripts/guestListFromCsv.js
+++ scripts/guestListFromCsv.js
@@ -1,19 +1,23 @@
-// Guess list url (google spreadsheet):
-// https://docs.google.com/spreadsheets/u/2/d/1vxAUOc8EpkhRpXNz0rsZcMOSlVt3fGTPKZgFcImRE64/edit?usp=drive_web&ouid=115217004423851001303
-// 
 // Example usage:
-// node ./scripts/guessListFromCsv.js csv-to-guesslist.csv > pages/guess_list.json
+// node ./scripts/guestListFromCsv.js guest_list.csv > pages/guess_list.json
+
 const fs = require("fs");
 const csv = require("csv-parser");
 
 const csvFile = process.argv[2];
 
-let guessList = [];
+let guestList = [];
 fs.createReadStream(csvFile)
   .pipe(csv())
   .on("data", function(data) {
-    guessList.push(JSON.stringify(data, null, 2));
+    guestList.push(data);
   })
   .on("end", function() {
-    console.log(`[${guessList.join(",")}]`);
+    const guestListJson = {
+      meta: {
+        lastUpdatedAt: new Date().toISOString()
+      },
+      data: guestList
+    };
+    console.log(JSON.stringify(guestListJson, null, 2));
   });
